ec3542984c4fd39ccecd0ab98abb9e2b7b392bbd
kishan.maharana <kishan.maharana@mail.vinove.com>
2022-08-21 21:45:59 +0530

    created local server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const http = require('node:http');
+
+const server = http.createServer((req,res)=>{
+    res.write('server started on port: 3000');
+    res.end();
+});
+
+server.listen(3000);
