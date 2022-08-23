4999b371f1f45c0993ac42aad2ae3bacd229e993
kishan.maharana <kishan.maharana@mail.vinove.com>
2022-08-23 22:21:13 +0530

    minor

--- Learning/app.js
+++ Learning/app.js
@@ -1,12 +1,12 @@
 
-const {area} = require('./circle');
-area();
+// const {area} = require('./circle');
+// area();
 
-// const http = require('node:http');
-
-// const server = http.createServer((req,res)=>{
-//     res.write('server started on port: 3000');
-//     res.end();
-// });
-
-// server.listen(3000);
+const http = require('node:http');
+const port = 4000;
+const server = http.createServer((req,res)=>{
+    res.write('running');
+    res.end();
+});
+console.log(`Server is running at http://localhost:${port}`)
+server.listen(port);
