d1b0b99960ff8c6d8dccef1095db3da1e3188f28
kishan.maharana <kishan.maharana@mail.vinove.com>
2022-08-23 21:50:31 +0530

    minor

---
+++ Learning/app.js
@@ -0,0 +1,12 @@
+
+const {area} = require('./circle');
+area();
+
+// const http = require('node:http');
+
+// const server = http.createServer((req,res)=>{
+//     res.write('server started on port: 3000');
+//     res.end();
+// });
+
+// server.listen(3000);
---
+++ Learning/circle.js
@@ -0,0 +1,6 @@
+
+exports.area = function(){
+    console.log('hello im from circle dot js fine'); 
+};
+// exports.area;
+// area();
